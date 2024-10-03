730872d1018520b8067dfa00974fe75532f2f2f7
Marcel Koonstra <marcelkoonstra@embracecloud.nl>
2024-10-03 06:10:04 +0200

    Form setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cijfers';
+  maxScore = 100;
+  standardization = "nterm";
+  nterm = 1;
+  passGrade = 6;
+  passScoreGoal = 60;
 }
